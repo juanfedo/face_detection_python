6b4025b1dbf4d3e45d8df1e3d9fbd78ac27a5c5d
Julian Fernandez <julian.fernandez@idu.gov.co>
2018-12-12 16:34:23 -0500

    se agrega multiproceso (aun no funciona) y contadores de tiempo

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from importlib import import_module
 import os
 from flask import Flask, render_template, Response,request
 from recognize_faces_video import VideoCamera
-
+from multiprocessing.pool import ThreadPool
+import os
+os.system("taskset -a -p {}".format(os.getpid()))
 
 # import camera driver
 # if os.environ.get('CAMERA'):
@@ -22,18 +24,19 @@ class App_Singleton:
     def getApplication():
         if App_Singleton.__application == None:
             App_Singleton.__application = VideoCamera()
-        return App_Singleton.__application 
-
+        return App_Singleton.__application
 
 app = Flask(__name__)
+pool = ThreadPool(processes=4)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def gen():
+    print('OS PID::::{}'.format(os.getpid()))
     while True:
-        frames = App_Singleton.getApplication().frames()        
+        frames = App_Singleton.getApplication().frames()
         for frame in frames:
             yield (b'--frame\r\n'
                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
@@ -43,8 +46,9 @@ def video_feed():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         print('REMOTE ADDRESS: ' + request.environ['REMOTE_ADDR'])
     else:
-        print('REMOTE ADDRESS: ' + request.environ['HTTP_X_FORWARDED_FOR']) 
-    return Response(gen(),
+        print('REMOTE ADDRESS: ' + request.environ['HTTP_X_FORWARDED_FOR'])
+    async_result = pool.apply_async(gen)
+    return Response(async_result.get(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
--- recognize_faces_video.py
+++ recognize_faces_video.py
@@ -5,84 +5,99 @@ import imutils
 import pickle
 import time
 import cv2
+from multiprocessing.pool import ThreadPool
+import os
+
+# os.system("taskset -p 0xfffff %d" % os.getpid())
 
 class VideoCamera():
 
-    def __init__(self):
-		self.encodings = '/home/usuario/Codigo/face_detection_python/encodings.pickle'
+	def __init__(self):
+		self.encodings = '/home/cjfernan1/face/face_detection_python/encodings.pickle'
 		print("[INFO] loading encodings...")
 		self.data = pickle.loads(open(self.encodings, "rb").read())
 		self.vs = VideoStream(src=0).start()
+		os.system("taskset -a -p {}".format(os.getpid()))
 		time.sleep(2.0)
 
-    def __del__(self):
-        self.vs.stop()
+	def __del__(self):
+		self.vs.stop()
 
-    def frames(self):
+	def worker(self,frame):
+		print (str(os.getpid()) + " is going to sleep...")
+
 		detection_method=''
-		print("[INFO] starting video stream...")		
+		# convert the input frame from BGR to RGB then resize it to have
+		# a width of 750px (to speedup processing)
+		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		rgb = imutils.resize(frame, width=750)
+		r = frame.shape[1] / float(rgb.shape[1])
+
+		# detect the (x, y)-coordinates of the bounding boxes
+		# corresponding to each face in the input frame, then compute
+		# the facial embeddings for each face
+		boxes = face_recognition.face_locations(rgb,
+			model=detection_method)
+		encodings = face_recognition.face_encodings(rgb, boxes)
+		names = []
+
+		# loop over the facial embeddings
+		for encoding in encodings:
+			# attempt to match each face in the input image to our known
+			# encodings
+			matches = face_recognition.compare_faces(self.data["encodings"],
+				encoding)
+			name = "Unknown"
+
+			# check to see if we have found a match
+			if True in matches:
+				# find the indexes of all matched faces then initialize a
+				# dictionary to count the total number of times each face
+				# was matched
+				matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+				counts = {}
+
+				# loop over the matched indexes and maintain a count for
+				# each recognized face face
+				for i in matchedIdxs:
+					name = self.data["names"][i]
+					counts[name] = counts.get(name, 0) + 1
+
+				# determine the recognized face with the largest number
+				# of votes (note: in the event of an unlikely tie Python
+				# will select first entry in the dictionary)
+				name = max(counts, key=counts.get)
+
+			# update the list of names
+			names.append(name)
+
+		# loop over the recognized faces
+		for ((top, right, bottom, left), name) in zip(boxes, names):
+			# rescale the face coordinates
+			top = int(top * r)
+			right = int(right * r)
+			bottom = int(bottom * r)
+			left = int(left * r)
+
+			# draw the predicted face name on the image
+			cv2.rectangle(frame, (left, top), (right, bottom),
+				(0, 255, 0), 2)
+			y = top - 15 if top - 15 > 15 else top + 15
+			cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+				0.75, (0, 255, 0), 2)
+		return frame
+
+	def frames(self):
+		print("[INFO] starting video stream...")
+		pool = ThreadPool(processes=2)
 		while True:
+			origin_time = time.time()
 			frame = self.vs.read()
-			
-			# convert the input frame from BGR to RGB then resize it to have
-			# a width of 750px (to speedup processing)
-			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-			rgb = imutils.resize(frame, width=750)
-			r = frame.shape[1] / float(rgb.shape[1])
+			#frame = self.worker(frame)
 
-			# detect the (x, y)-coordinates of the bounding boxes
-			# corresponding to each face in the input frame, then compute
-			# the facial embeddings for each face
-			boxes = face_recognition.face_locations(rgb,
-				model=detection_method)
-			encodings = face_recognition.face_encodings(rgb, boxes)
-			names = []
+			async_result = pool.apply_async(self.worker, (frame, ))
+			frame = async_result.get()
 
-			# loop over the facial embeddings
-			for encoding in encodings:
-				# attempt to match each face in the input image to our known
-				# encodings
-				matches = face_recognition.compare_faces(self.data["encodings"],
-					encoding)
-				name = "Unknown"
-
-				# check to see if we have found a match
-				if True in matches:
-					# find the indexes of all matched faces then initialize a
-					# dictionary to count the total number of times each face
-					# was matched
-					matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-					counts = {}
-
-					# loop over the matched indexes and maintain a count for
-					# each recognized face face
-					for i in matchedIdxs:
-						name = self.data["names"][i]
-						counts[name] = counts.get(name, 0) + 1
-
-					# determine the recognized face with the largest number
-					# of votes (note: in the event of an unlikely tie Python
-					# will select first entry in the dictionary)
-					name = max(counts, key=counts.get)
-				
-				# update the list of names
-				names.append(name)
-
-			# loop over the recognized faces
-			for ((top, right, bottom, left), name) in zip(boxes, names):
-				# rescale the face coordinates
-				top = int(top * r)
-				right = int(right * r)
-				bottom = int(bottom * r)
-				left = int(left * r)
-
-				# draw the predicted face name on the image
-				cv2.rectangle(frame, (left, top), (right, bottom),
-					(0, 255, 0), 2)
-				y = top - 15 if top - 15 > 15 else top + 15
-				cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-					0.75, (0, 255, 0), 2)
-
-			#print ('FRAME ENTREGADO')
-			#return cv2.imencode('.jpg', frame)[1].tobytes()
+			time_interval = time.time() - origin_time
+			print('TIEMPO {}'.format(time_interval))
 			yield cv2.imencode('.jpg', frame)[1].tobytes()
